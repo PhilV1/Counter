4b1f6be2320f0e2daf70a088676d1821c377eff1
PhilV1 <werpub.phil@gmx.de>
2024-04-01 16:42:25 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const yourName = prompt('What is your name?');
+console.log(yourName);
+
+const number = document.querySelector('.number');
+const increase = document.querySelector('.increase');
+const decrease = document.querySelector('.decrease');
+const reset = document.querySelector('.reset');
+let numberCount = 0;
+console.log(numberCount);
+
+// Increase Number
+increase.addEventListener('click', function () {
+  numberCount++;
+  number.textContent = numberCount;
+  if (numberCount > 0) {
+    number.style.color = 'green';
+  } else if (numberCount === 0) {
+    number.style.color = 'black';
+  }
+});
+
+// Decrease Number
+decrease.addEventListener('click', function () {
+  numberCount--;
+  number.textContent = numberCount;
+  if (numberCount < 0) {
+    number.style.color = 'red';
+  } else if (numberCount === 0) {
+    number.style.color = 'black';
+  }
+});
+
+// Reset Button
+reset.addEventListener('click', function () {
+  numberCount = 0;
+  number.textContent = numberCount;
+  number.style.color = 'black';
+});
