715107570e28975d784aedbc740b8ee522c713bc
PhilV1 <werpub.phil@gmx.de>
2024-04-01 16:43:54 +0200

    Refactor script.js

--- script.js
+++ script.js
@@ -1,40 +1,37 @@
-'use strict';
+'use strict'
 
-const yourName = prompt('What is your name?');
-console.log(yourName);
-
-const number = document.querySelector('.number');
-const increase = document.querySelector('.increase');
-const decrease = document.querySelector('.decrease');
-const reset = document.querySelector('.reset');
-let numberCount = 0;
-console.log(numberCount);
+const number = document.querySelector('.number')
+const increase = document.querySelector('.increase')
+const decrease = document.querySelector('.decrease')
+const reset = document.querySelector('.reset')
+let numberCount = 0
+console.log(numberCount)
 
 // Increase Number
 increase.addEventListener('click', function () {
-  numberCount++;
-  number.textContent = numberCount;
+  numberCount++
+  number.textContent = numberCount
   if (numberCount > 0) {
-    number.style.color = 'green';
+    number.style.color = 'green'
   } else if (numberCount === 0) {
-    number.style.color = 'black';
+    number.style.color = 'black'
   }
-});
+})
 
 // Decrease Number
 decrease.addEventListener('click', function () {
-  numberCount--;
-  number.textContent = numberCount;
+  numberCount--
+  number.textContent = numberCount
   if (numberCount < 0) {
-    number.style.color = 'red';
+    number.style.color = 'red'
   } else if (numberCount === 0) {
-    number.style.color = 'black';
+    number.style.color = 'black'
   }
-});
+})
 
 // Reset Button
 reset.addEventListener('click', function () {
-  numberCount = 0;
-  number.textContent = numberCount;
-  number.style.color = 'black';
-});
+  numberCount = 0
+  number.textContent = numberCount
+  number.style.color = 'black'
+})
